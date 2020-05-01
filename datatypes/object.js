13f61e34b7a54b23ed7bd5ec8afab73619941264
koko1313 <kaloyanvellichkov@gmail.com>
2020-05-01 20:41:12 +0300

    object updated

--- datatypes/object.js
+++ datatypes/object.js
@@ -1,4 +1,5 @@
-var x = {}; // empty Object
+var x = new Object(); // define empty Object
+var x = {}; // second way to define empty Object
 
 // Object with properties
 var x = {
@@ -8,3 +9,16 @@ var x = {
 
 // add a property to object
 x.prop3 = "prop3 value";
+
+
+/**
+ * Functions are also objects
+ */
+var Pizza = function() {
+    this.crust = "thin",
+    this.hasCheese = true;
+}
+
+var pizza1 = new Pizza();
+
+console.log(pizza1);
