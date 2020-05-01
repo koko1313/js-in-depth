44302fb2fbc7fbee136f7f6de9acf5be1f43dee2
koko1313 <kaloyanvellichkov@gmail.com>
2020-05-01 22:30:58 +0300

    functions are also objects

--- datatypes/object.js
+++ datatypes/object.js
@@ -15,10 +15,16 @@ x.prop3 = "prop3 value";
  * Functions are also objects
  */
 var Pizza = function() {
-    this.crust = "thin",
-    this.hasCheese = true;
+    this.crust = "thin"; // public property
+    var hasCheese = true; // private property
+
+    this.getHashCheese = function() {
+        return hasCheese;
+    }
 }
 
 var pizza1 = new Pizza();
 
-console.log(pizza1);
+console.log(pizza1.crust); // thin
+
+console.log(pizza1.getHashCheese()); // true
